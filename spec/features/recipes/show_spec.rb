fe3bce1ec576ad87e1894af481c12b9941f9ff54
mkbonini <mkbonini@gmail.com>
2022-07-18 14:39:52 -0600

    test: recipe show page should have total cost

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -20,4 +20,19 @@ RSpec.describe 'recipe show page' do
         expect(page).to have_content("grilled chicken")
         expect(page).to_not have_content("white bread")
     end
+
+    it 'gives total cost of all ingredients' do
+        pasta = Recipe.create!(name: 'spaghetti', complexity: 1, genre: 'italian')
+        noodles = Ingredient.create!(name: 'spaghetti noodles', cost: 2)
+        chicken = Ingredient.create!(name: 'grilled chicken', cost: 3)
+        sauce = Ingredient.create!(name: 'red sauce', cost: 4)
+
+        recipe_ingredient_1 = RecipeIngredient.create!(recipe_id: pasta.id, ingredient_id: noodles.id)
+        recipe_ingredient_2 = RecipeIngredient.create!(recipe_id: pasta.id, ingredient_id: chicken.id)
+        recipe_ingredient_3 = RecipeIngredient.create!(recipe_id: pasta.id, ingredient_id: sauce.id)
+
+        visit "/recipes/#{pasta.id}"
+        
+        expect(page).to have_content("Total Cost: 9")
+    end
 end
